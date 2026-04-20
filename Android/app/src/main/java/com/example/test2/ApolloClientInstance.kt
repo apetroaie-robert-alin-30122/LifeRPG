caafa4bb20557cc748290296c8de7de0d63aa871
KiriKittykee <150711589+KiriKittykee@users.noreply.github.com>
2026-04-20 14:43:44 +0300

    Actualizat IP-ul serverului pt emulator (10.0.2.2). :D

--- Android/app/src/main/java/com/example/test2/ApolloClientInstance.kt
+++ Android/app/src/main/java/com/example/test2/ApolloClientInstance.kt
@@ -4,6 +4,6 @@ import com.apollographql.apollo.ApolloClient
 
 object ApolloClientInstance {
     val client = ApolloClient.Builder()
-        .serverUrl("http://127.0.0.1:8000//graphql")
+        .serverUrl("http://10.0.2.2:8000/graphql")
         .build()
 }
